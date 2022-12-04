022a90257ffd4c432bc7e594d49a6f3920e929dc
alexyao2015 <33379584+alexyao2015@users.noreply.github.com>
2022-12-04 13:54:08 -0600

    Make title optional too

--- python_scripts/send_android_tv_notifications.py
+++ python_scripts/send_android_tv_notifications.py
@@ -3,12 +3,13 @@ image_url = data.get("image_url")
 title = data.get("title")
 message = data.get("message")
 
-if services and title and message:
+if services and message:
     for service in services:
-        service_data = {"message": message, "title": title}
+        service_data = {"message": message}
         if image_url:
             service_data["data"] = {"image": {"url": image_url}}
-
+        if title:
+            service_data["title"] = title
         hass.services.call("notify", service, service_data)
 else:
     logger.error(f"Not all required parameters were provided. message: {message}, title: {title}, image_url: {image_url}, services: {services}")
