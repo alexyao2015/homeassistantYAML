9d0de13da83c6d6f3053375e1191d70f7492c869
alexyao2015 <33379584+alexyao2015@users.noreply.github.com>
2022-12-04 13:49:46 -0600

    Optional image url

--- python_scripts/send_android_tv_notifications.py
+++ python_scripts/send_android_tv_notifications.py
@@ -3,9 +3,12 @@ image_url = data.get("image_url")
 title = data.get("title")
 message = data.get("message")
 
-if services and image_url and title and message:
+if services and title and message:
     for service in services:
-        service_data = {"message": message, "title": title, "data": {"image": {"url": image_url}}}
+        service_data = {"message": message, "title": title}
+        if image_url:
+            service_data["data"] = {"image": {"url": image_url}}
+
         hass.services.call("notify", service, service_data)
 else:
     logger.error(f"Not all required parameters were provided. message: {message}, title: {title}, image_url: {image_url}, services: {services}")
