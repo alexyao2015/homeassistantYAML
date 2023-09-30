ec9db2f364bf91233e76b39de005ce48d1147241
alexyao2015 <33379584+alexyao2015@users.noreply.github.com>
2023-09-30 12:27:27 -0500

    Upgrade blueprints to new filter multiple

--- python_scripts/send_android_tv_notifications.py
+++ python_scripts/send_android_tv_notifications.py
@@ -5,11 +5,13 @@ message = data.get("message")
 
 if services and message:
     for service in services:
+        # domain should always be notify
+        domain, service_name = service.split(".")
         service_data = {"message": message}
         if image_url:
             service_data["data"] = {"image": {"url": image_url}}
         if title:
             service_data["title"] = title
-        hass.services.call("notify", service, service_data)
+        hass.services.call(domain, service_name, service_data)
 else:
     logger.error(f"Not all required parameters were provided. message: {message}, title: {title}, image_url: {image_url}, services: {services}")
